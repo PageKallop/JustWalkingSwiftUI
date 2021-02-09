c5a994e24986fffafdb256c1c21c229835852540
PageKallop <PageMarie78@gmail.com>
2021-02-09 14:07:08 -0600

    First commit

--- JustWalkingSwiftUI/ContentView.swift
+++ JustWalkingSwiftUI/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
+    
+    private let pedometer: CMPedometer = CMPedometer()
+    
+    @State private var steps: Int?
+    
+    private var isPedometerAvailable: Bool {
+        return CMPedometer.isPedometerEventTrackingAvailable() &&
+            CMPedometer.isDistanceAvailable() && CMPedometer.isStepCountingAvailable()
+    }
+    
+    private func initializePedometer() {
+        if isPedometerAvailable {
+            guard let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date()) else {
+                return
+            }
+            
+            pedometer.queryPedometerData(from: startDate, to: Date()) { (data, error) in
+                guard let data = data, error == nil else { return }
+                
+                steps = data.numberOfSteps.intValue
+            }
+        }
+        
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(steps != nil ? "\(steps!)" : "")
             .padding()
+            .onAppear() {
+                initializePedometer()
+            }
     }
 }
 
