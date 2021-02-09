8b062a887b9e9556587cbec2b042705ef3f62bca
PageKallop <PageMarie78@gmail.com>
2021-02-09 14:44:39 -0600

    Added widget

--- JustWalkingSwiftUI/ContentView.swift
+++ JustWalkingSwiftUI/ContentView.swift
@@ -14,9 +14,23 @@ struct ContentView: View {
     
     @State private var steps: Int?
     
+    @State private var distance: Double?
+    
     private var isPedometerAvailable: Bool {
         return CMPedometer.isPedometerEventTrackingAvailable() &&
             CMPedometer.isDistanceAvailable() && CMPedometer.isStepCountingAvailable()
+    }
+    
+    private func updateUI(data: CMPedometerData) {
+        
+        steps = data.numberOfSteps.intValue
+        
+        guard let pedometerDistance
+                = data.distance else { return }
+        let distanceInMeters = Measurement(value: Double(truncating: pedometerDistance), unit: UnitLength.meters)
+         
+        distance = distanceInMeters.converted(to: .miles).value
+        
     }
     
     private func initializePedometer() {
@@ -28,14 +42,17 @@ struct ContentView: View {
             pedometer.queryPedometerData(from: startDate, to: Date()) { (data, error) in
                 guard let data = data, error == nil else { return }
                 
+                updateUI(data: data)
                 steps = data.numberOfSteps.intValue
+                distance = data.distance?.doubleValue
             }
         }
         
     }
     
     var body: some View {
-        Text(steps != nil ? "\(steps!)" : "")
+        Text(steps != nil ? "\(steps!) steps" : "")
+        Text(distance != nil ? String(format: "%.f miles", distance!) : "")
             .padding()
             .onAppear() {
                 initializePedometer()
